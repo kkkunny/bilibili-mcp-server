cf240b4f71246249d58cb194b944ecdac9bf416b
kkkunny <1401289859@qq.com>
2025-09-27 00:17:32 +0800

    feat: port 8080=>80

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	mapSvr := server.NewMCPServer("BiliBili", "0.0.1", server.WithLogging(), server.WithRecovery())
 	mapSvr.AddTools(tools.Tools...)
 	httpSvr := server.NewStreamableHTTPServer(mapSvr)
-	if err := httpSvr.Start(":8080"); err != nil {
+	if err := httpSvr.Start(":80"); err != nil {
 		panic(err)
 	}
 }
